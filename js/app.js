b26b63138dcb49ef0c8a0665897ab0dc44d29dbc
Diego Sebastián <diegosebastian.dev@gmail.com>
2018-06-24 10:58:08 -0400

    Correciones 1

---
+++ js/app.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+  $('.galeria-notex-1').slick({
+  dots: true,
+  autoplaySpeed: 5000,
+  infinite: true,
+  autoplay: true,
+  slidesToShow: 1,
+	});
+
+  $('.galeria-notex-2').slick({
+  autoplay: true,
+  autoplaySpeed: 5000,
+  dots: true,
+  infinite: true,
+  slidesToShow: 1,
+	});
+
+  $('.galeria-notex-3').slick({
+  autoplay: true,
+  autoplaySpeed: 5000,
+  dots: true,
+  infinite: true,
+  slidesToShow: 1,
+	});
+
+  $('.galeria-home').slick({
+  autoplay: true,
+  autoplaySpeed: 5000,
+  dots: true,
+  infinite: true,
+  slidesToShow: 1,
+	});
+
+});
